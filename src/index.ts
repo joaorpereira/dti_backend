860e74ffc21669a867efcf0721e4c93eefa8cd2e
joaorpereira <jprp17@live.com>
2021-03-20 09:32:23 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Express } from 'express'
+import { AddressInfo } from 'net'
+import cors from 'cors'
+
+const app: Express = express()
+
+app.use(cors())
+app.use(express.json())
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo
+    console.log(`Serving running in http://localhost: ${address.port}`)
+  } else {
+    console.error(`Failure starting server`)
+  }
+})
